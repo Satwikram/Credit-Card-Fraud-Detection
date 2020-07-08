18b0073dc50dc3da38585ecf2287dde2839c4885
Satwik Ram <satwikram29@gmail.com>
2020-07-08 15:53:30 +0530

    updated

--- fruad/views.py
+++ fruad/views.py
@@ -46,7 +46,7 @@ def predict(request):
         v27 = float(request.POST['v27'])
         v28 = float(request.POST['v28'])
 
-        pred = model.predict(np.array([time1, amount1, v1, v2, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14,
+        pred = model.predict(np.array([time1, amount1, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14,
                                        v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28]))
 
 
