c7a46f246a9437fbaa283824862d8ee52c4d40e4
Satwik Ram <satwikram29@gmail.com>
2020-07-08 13:14:58 +0530

    updated

--- fruad/views.py
+++ fruad/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 import json
+import joblib
+import numpy as np
 
 # Create your views here.
 
-
-
 def predict(request):
-    if request.method == 'POST':
-        pass
+    if request.json is not None:
+
+        model = joblib.load('model.sav')
+        scaler = joblib.load('scaler.sav')
+
+        data = json.loads(request.body)
+        print(data)
+
